22700fd687e0b7e032ed9291077e3b9fc7d68d45
ChauHaiDang1 <chauhaidang1@gmail.com>
2019-07-19 17:20:47 +0700

    Session 5 - install mocha framework - setup test runner file

---
+++ test.config.js
@@ -0,0 +1,16 @@
+//Test runner will find the object with name "config", so we have to exports (feature from nodejs) object config for that runner
+exports.config = {
+    /** Config server running test */
+
+    runner: 'local', //Where to run
+    hostname: 'localhost',
+    port: 9515, 
+    path: '//',
+
+    /** Config Test Framework */
+    framework: 'mocha',
+    mochaOpts: {
+        ui: 'bdd' //running mode
+    }
+    
+}
