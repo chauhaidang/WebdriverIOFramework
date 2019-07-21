2f6fcfc552966f27d85bb7f9a1489f1c3884aaa4
Đăng Châu <chauhaidang1@gmail.com>
2019-07-21 15:36:27 +0700

    Session 7: add wdio cli, chromedriver, wdio sync

--- TC_001_Login.js
+++ TC_001_Login.js
@@ -1,5 +1,6 @@
 describe('Login Function', () => {
     it('Should login with correct creds', () => {
-        
+        browser.url("https://google.com");
+        browser.pause(2000);
     });
 });
--- test.config.js
+++ test.config.js
@@ -1,5 +1,17 @@
 //Test runner will find the object with name "config", so we have to exports (feature from nodejs) object config for that runner
 exports.config = {
+
+    /** Test script */
+    specs:[
+        "TC_001_Login.js"
+    ],
+
+    capabilities: [
+        {
+            browserName: "chrome"
+        }
+    ],
+
     /** Config server running test */
 
     runner: 'local', //Where to run
